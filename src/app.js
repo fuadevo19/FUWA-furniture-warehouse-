ff8ac8519e2a5207fc3c1d6e5093617bb840efd9
nezalramadhan <129980691+nezalramadhan@users.noreply.github.com>
2024-05-30 09:08:02 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const sequelize = require("./config/database");
 const cors = require("cors");
 const db = require("./models");
