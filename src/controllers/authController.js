b7e95af74ee3bc657ebedb76fdf6a44eaac549b7
Muhammad Fuadi <fuadevo19@gmail.com>
2024-06-15 21:22:59 +0700

    menambahkan respon username dari login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -18,7 +18,7 @@ const login = async (req, res) => {
       expiresIn: "1h",
     });
 
-    res.json({ token });
+    res.json({ token, username: user.username });
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
   }
